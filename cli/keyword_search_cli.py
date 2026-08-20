c2abf83b831704c75fa577e910e30e5c1afbd171
ajr-cabbage <alexander.rakotz@gmail.com>
2026-08-19 23:47:20 -0400

    CH2 complete

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,22 +2,27 @@ import argparse
 from typing import Any
 
 from nltk.sem.evaluate import sys
-from lib.search_utils import build_command, search_command, load_movies, load_stop_words, InvertedIndex
+from lib.search_utils import build_command, idf_command, search_command, tf_command, InvertedIndex, tfidf_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_parser = subparsers.add_parser("search", help="Search movies using keywords")
-    search_parser.add_argument("query", type=str, help="Search query")
+    _ = search_parser.add_argument("query", type=str, help="Search query")
     build_parser = subparsers.add_parser("build", help="Build inverted index")
+    tf_parser = subparsers.add_parser("tf", help="Print term frequency in a doc for a given term")
+    _ = tf_parser.add_argument("doc_id", type=int, help="Document ID")
+    _ = tf_parser.add_argument("term", type=str, help="freq match term")
+    idf_parser = subparsers.add_parser("idf", help="Print inter document frequency")
+    _ = idf_parser.add_argument("term", type=str, help="idf match term")
+    tfidf_parser = subparsers.add_parser("tfidf", help="Print TF-IDF value")
+    _ = tfidf_parser.add_argument("doc_id", type=int, help="Document ID")
+    _ = tfidf_parser.add_argument("term", type=str, help="tf-idf match term")
 
     args = parser.parse_args()
 
     movies_dat_filepath = "./data/movies.json"
-    stop_words_filepath = "./data/stopwords.txt"
-    movies_list = load_movies(movies_dat_filepath)
-    stop_words = load_stop_words(stop_words_filepath)
 
     inv_index = InvertedIndex()
 
@@ -28,8 +33,13 @@ def main() -> None:
         case "build":
             print(f"Building index from {movies_dat_filepath} ...")
             build_command(inv_index, movies_dat_filepath)
-            # s = list(inv_index.index["merida"])
-            # print(f"First document for token 'merida' = {s[0]}")
+        case "tf":
+            print(f"Returning frequency for {args.term} in {args.doc_id}")
+            tf_command(args.doc_id, args.term, inv_index)
+        case "idf":
+            idf_command(args.term, inv_index)
+        case "tfidf":
+            tfidf_command(args.doc_id, args.term, inv_index)
         case _:
             parser.print_help()
 
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -1,3 +1,6 @@
+from ast import Invert
+from collections import Counter
+import math
 import string
 import json
 import pickle
@@ -9,20 +12,32 @@ from nltk.stem import PorterStemmer
 class InvertedIndex:
     index: dict[str, set[int]]
     docmap: dict[int, Any]
+    term_frequencies: dict[int, Counter[str]]
 
     def __init__(self) -> None:
         self.index = {}
         self.docmap = {}
+        self.term_frequencies = {}
         self.stop_words: list[str] = load_stop_words("./data/stopwords.txt")
 
     def __add_document(self, doc_id: int, text: str) -> None:
         tokens = stem_tokens(filter_stop_tokens(tokenize_string(text), self.stop_words))
+        if doc_id in self.term_frequencies:
+            self.term_frequencies[doc_id].update(tokens)
+        else:
+            self.term_frequencies[doc_id] = Counter()
+            self.term_frequencies[doc_id].update(tokens)
         for token in tokens:
             if token in self.index:
                 self.index[token].add(doc_id)
             else:
                 self.index[token] = set()
                 self.index[token].add(doc_id)
+            #if doc_id in self.term_frequencies:
+            #    self.term_frequencies[doc_id].update([token])
+            #else:
+            #    self.term_frequencies[doc_id] = Counter()
+            #    self.term_frequencies[doc_id].update([token])
 
     def get_documents(self, term) -> list[int]:
         return sorted(self.index[term])
@@ -38,10 +53,13 @@ class InvertedIndex:
         cache_dir.mkdir(parents=True, exist_ok=True)
         index_filepath = Path(cache_dir, "index.pkl")
         docmap_filepath = Path(cache_dir, "docmap.pkl")
+        term_freq_filepath = Path(cache_dir, "term_frequencies.pkl")
         with open(index_filepath, "wb") as file:
             pickle.dump(self.index, file)
         with open(docmap_filepath, "wb") as file:
             pickle.dump(self.docmap, file)
+        with open(term_freq_filepath, "wb") as file:
+            pickle.dump(self.term_frequencies, file)
 
     def load(self) -> None:
         index_path = Path("./cache/index.pkl")
@@ -50,10 +68,38 @@ class InvertedIndex:
         docmap_path = Path("./cache/docmap.pkl")
         if not docmap_path.is_file():
             raise FileNotFoundError("docmap file not found")
+        term_freq_filepath = Path("./cache/term_frequencies.pkl")
+        if not term_freq_filepath.is_file():
+            raise FileNotFoundError("term_frequencies file not found")
         with open(index_path, "rb") as file:
             self.index = pickle.load(file)
         with open(docmap_path, "rb") as file:
             self.docmap = pickle.load(file)
+        with open(term_freq_filepath, "rb") as file:
+            self.term_frequencies = pickle.load(file)
+
+    def get_tf(self, doc_id: int, term: str) -> int:
+        if doc_id in self.term_frequencies:
+            return self.term_frequencies[doc_id].get(term, 0)
+        return -1
+
+    def get_idf(self, term: str) -> float:
+        return math.log((len(self.docmap)+1) / (len(self.index[term]) + 1))
+
+def load_stop_words(filepath: str) -> list[str]:
+    with open(filepath, "r") as file:
+        content = file.read()
+        translate_table = str.maketrans("", "", string.punctuation)
+        stop_words = content.translate(translate_table).lower().splitlines()
+    return stop_words
+
+stop_tokens = load_stop_words("./data/stopwords.txt")
+
+def tokenize_term(term: str) -> str:
+    term_token = stem_tokens(filter_stop_tokens(tokenize_string(term), stop_tokens))
+    if len(term_token) != 1:
+        raise ValueError("term length is not 1")
+    return term_token[0]
 
 
 def tokenize_string(input: str) -> list[str]:
@@ -94,13 +140,6 @@ def load_movies(filepath: str) -> list[Any]:
         movies_dat = json.loads(content)
     return movies_dat["movies"]
 
-def load_stop_words(filepath: str) -> list[str]:
-    with open(filepath, "r") as file:
-        content = file.read()
-        translate_table = str.maketrans("", "", string.punctuation)
-        stop_words = content.translate(translate_table).lower().splitlines()
-    return stop_words
-
 def search_command(query: str, inv_index: InvertedIndex) -> None:
     try:
         inv_index.load()
@@ -108,7 +147,6 @@ def search_command(query: str, inv_index: InvertedIndex) -> None:
         print(e)
         sys.exit(1)
     query_tokens = stem_tokens(filter_stop_tokens(tokenize_string(query), inv_index.stop_words))
-    item_number = 1
     matching_ids: list[int] = []
     for query_token in query_tokens:
         if query_token in inv_index.index:
@@ -125,3 +163,43 @@ def search_command(query: str, inv_index: InvertedIndex) -> None:
 def build_command(inv_index: InvertedIndex, movies_dat_filepath: str):
     inv_index.build(movies_dat_filepath)
     inv_index.save()
+
+def tf_command(doc_id: int, term: str, inv_index: InvertedIndex):
+    try:
+        inv_index.load()
+    except FileNotFoundError as e:
+        print(e)
+        sys.exit(1)
+    try:
+        tok = tokenize_term(term)
+    except ValueError as e:
+        print(e)
+        return
+    print(inv_index.get_tf(doc_id, tok))
+
+def idf_command(term: str, inv_index: InvertedIndex):
+    try:
+        inv_index.load()
+    except FileNotFoundError as e:
+        print(e)
+        sys.exit(1)
+    try:
+        tok = tokenize_term(term)
+    except ValueError as e:
+        print(e)
+        return
+    print(f"Inverse document frequency of '{term}': {inv_index.get_idf(tok):.2f}")
+
+def tfidf_command(doc_id: int, term: str, inv_index: InvertedIndex):
+    try:
+        inv_index.load()
+    except FileNotFoundError as e:
+        print(e)
+        sys.exit(1)
+    try:
+        tok = tokenize_term(term)
+    except ValueError as e:
+        print(e)
+        return
+    tfidf_score: float = inv_index.get_tf(doc_id, tok) * inv_index.get_idf(tok)
+    print(f"TF-IDF score of '{term}' in document '{doc_id}': {tfidf_score:.2f}")
